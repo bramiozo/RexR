ed6979eefa03f3cf5906e666d46e0cbf63442d2d
bramiozo <bramiozo@gmail.com>
2018-02-24 13:01:01 +0100

    update

---
+++ _doc/melanoma_set2.md
@@ -0,0 +1,12 @@
+# Melanoma set 2
+
+ingenuity pathway analysis (IPA)
+
+pathways/processen
+
+QIAGEN
+
+regulators
+
+diseases/bio functions
+
--- functions/predictor_ebe.py
+++ functions/predictor_ebe.py
@@ -1,5 +1,5 @@
-# Cluster Enhanced Biased Estimator
-
+# Cluster Enhanced Convex Biased Estimator
+# binomial classification
 
 class CEBE():
 	def __init__(self, clustering = "SOM", dim_reduction = None, aggregations = None, classifier = "LSVM"):
@@ -10,21 +10,25 @@ class CEBE():
 
 
 
+
 # Dim. reduction
 ####################################
-# PCA/LDA/PLS/SAE
+# Variance filter per cluster?
 
 
 
 # Sample aggregations per cluster
 ####################################
 # percentiles: 10, 25, 50, 75, 90, modes: peaks in density estimations
+# percentiles per class, per feature, per cluster,
+# [cluster_n, classification_m, percentiles, ..]
 
 
 
-# Classification
+# Convex Hulls
 ####################################
-# LSVM, LR
+# 
+# [hull_k, cluster_n, classification_m, hull_coordinates_mean, hull_coordinates_median, hull_coordinates_variance]
 
 
 
