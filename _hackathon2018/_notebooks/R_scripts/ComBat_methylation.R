30576d3cea343ca38df0ab71e87e69c03be2b895
Bram van Es <bramiozo@gmail.com>
2019-03-21 18:58:13 +0100

    update

--- _hackathon2018/_notebooks/R_scripts/ComBat_methylation.R
+++ _hackathon2018/_notebooks/R_scripts/ComBat_methylation.R
@@ -1,5 +1,12 @@
 # https://bioconductor.org/packages/release/bioc/vignettes/sva/inst/doc/sva.pdf
 
+##### 
+type_data <- read.table("/media/koekiemonster/DATA-FAST/genetic_expression/hackathon_2/Lung/MethylationProbeTypes.csv", 
+                        sep="\t", header=TRUE)
+TypeI = as.character(type_data[type_data['Infinium_Design_Type']=='I', 'IlmnID'])
+TypeII = as.character(type_data[type_data['Infinium_Design_Type']=='II', 'IlmnID'])
+
+
 # keep only primary tumor
 pheno_data <- read.table("/media/koekiemonster/DATA-FAST/genetic_expression/hackathon_2/Lung/_prepped/methylation_combat_meta_small_cont.csv",
                          sep="\t", header=TRUE)
@@ -36,7 +43,7 @@ modcombat<-NULL
 for(i in 1:ncol(pheno_data)){
   pheno_data[is.na(pheno_data[,i]), i] <- mean(pheno_data[,i], na.rm = TRUE)
 }
-modcombat = model.matrix(~as.factor(cat_gender)+as.numeric(cat_age)+as.numeric(cat_packs), data=pheno_data) # +as.numeric(cat_age)+as.numeric(cat_packs), cat_smoke_years
+modcombat = model.matrix(~as.factor(cat_gender), data=pheno_data) # +as.numeric(cat_age)+as.numeric(cat_packs), cat_smoke_years
 modcombat1 = model.matrix(~as.factor(cat_gender), data=pheno1)
 modcombat2 = model.matrix(~as.factor(cat_gender), data=pheno2)
 
@@ -53,12 +60,17 @@ full_data <- full_data[!duplicated(full_data[[1]]),]
 rownames(full_data) <- full_data[,1]
 full_data[,1]<- NULL 
 
+full_rows = rownames(full_data)
+final_TypeI = Reduce(intersect, list(full_rows, TypeI))
+final_TypeII = Reduce(intersect, list(full_rows, TypeII))
+
 full_data1 <- full_data[, diag1]
 full_data2 <- full_data[, diag2] 
 
 full_matrix = as.matrix(full_data)
-full_matrix1 = as.matrix(full_data1)
-full_matrix2 = as.matrix(full_data2)
+
+
+
 
 # make sure that the order of the samples is the same !!
 ####
@@ -71,50 +83,91 @@ source("/home/koekiemonster/DEV/GIT/RexR/_hackathon2018/_notebooks/R_scripts/CCo
 #            row.names=TRUE)
 
 nullmode<-FALSE
-splitByType<-FALSE
+splitByTarget<-FALSE
+splitByType<-TRUE
 param<-FALSE
+
+if((splitByTarget==TRUE) & (splitByType==FALSE)){
+  print("Split by target")
+  full_matrix1 = as.matrix(full_data1)
+  full_matrix2 = as.matrix(full_data2)
+  #rm(full_matrix)
+  #rm(full_data)
+}else if((splitByTarget==FALSE)  &  (splitByType==TRUE)){
+  print("Split by type")
+  full_matrix1 = as.matrix(full_data[final_TypeI,])
+  full_matrix2 = as.matrix(full_data[final_TypeII,])
+  #rm(full_matrix)
+  #rm(full_data)
+}else if((splitByTarget==TRUE)  &  (splitByType==TRUE)){
+  print("Split by target and  type")
+  full_matrix11 = as.matrix(full_data1[final_TypeI,])
+  full_matrix21 = as.matrix(full_data1[final_TypeII,])  
+  full_matrix12 = as.matrix(full_data2[final_TypeI,])
+  full_matrix22 = as.matrix(full_data2[final_TypeII,]) 
+  #rm(full_matrix)
+  #rm(full_data)
+  }
+
+  
+
 if(param){parstring='param'}else{parstring='nonparam'}
 
-
-  if (nullmode==TRUE){
-    if (splitByType==FALSE){
-        corrected_batch_only_par = CComBat(full_matrix, batch = batch, mod = mod0, par.prior = param)
+    if (splitByTarget==FALSE && splitByType==FALSE){
+      print("Full matrix")
+        if(nullmode==FALSE){mod = modcombat}else{mod = mod0}
+        corrected_batch_only_par = CComBat(full_matrix, batch = batch, mod = mod, par.prior = param)
         write.table(corrected_batch_only_par, 
                     file=paste("/media/koekiemonster/DATA-FAST/genetic_expression/hackathon_2/Lung/_prepped/methylation_combat_0nans_primaryonly_",
                                parstring, ".csv", sep=""),
                     row.names=TRUE)
+    }else if (splitByTarget==TRUE && splitByType==TRUE){
+      print("Split by target and type")
+      if(nullmode==FALSE){mod = modcombat1}else{mod = mod01}
+      corrected_batch_only_par = CComBat(full_matrix11, batch = batch1, mod = mod, par.prior = param)
+      write.table(corrected_batch_only_par, 
+                  file=paste("/media/koekiemonster/DATA-FAST/genetic_expression/hackathon_2/Lung/_prepped/methylation_combat_0nans_primaryonly_",
+                             parstring, "_target1_type1.csv", sep=""),
+                  row.names=TRUE)
+      
+      if(nullmode==FALSE){mod = modcombat1}else{mod = mod01}
+      corrected_batch_only_par = CComBat(full_matrix21, batch = batch1, mod = mod, par.prior = param)
+      write.table(corrected_batch_only_par, 
+                  file=paste("/media/koekiemonster/DATA-FAST/genetic_expression/hackathon_2/Lung/_prepped/methylation_combat_0nans_primaryonly_",
+                             parstring, "_target1_type2.csv", sep=""),
+                  row.names=TRUE)   
+      
+      if(nullmode==FALSE){mod = modcombat2}else{mod = mod02}
+      corrected_batch_only_par = CComBat(full_matrix12, batch = batch2, mod = mod, par.prior = param)
+      write.table(corrected_batch_only_par, 
+                  file=paste("/media/koekiemonster/DATA-FAST/genetic_expression/hackathon_2/Lung/_prepped/methylation_combat_0nans_primaryonly_",
+                             parstring, "_target2_type1.csv", sep=""),
+                  row.names=TRUE)
+      
+      if(nullmode==FALSE){mod = modcombat2}else{mod = mod02}
+      corrected_batch_only_par = CComBat(full_matrix22, batch = batch2, mod = mod, par.prior = param)
+      write.table(corrected_batch_only_par, 
+                  file=paste("/media/koekiemonster/DATA-FAST/genetic_expression/hackathon_2/Lung/_prepped/methylation_combat_0nans_primaryonly_",
+                             parstring, "_target2_type2.csv", sep=""),
+                  row.names=TRUE)       
     }else{
-        corrected_batch_only_par = CComBat(full_matrix1, batch = batch1, mod = mod01, par.prior = param)
+        if(splitByTarget==TRUE){tstring<-'target'}else{tstring<-'type'}
+      
+        print(paste("Split by", tstring, sep=" "))
+        
+        if(nullmode==FALSE){if(splitByTarget==TRUE){mod = modcombat1; tbatch=batch1}else{mod = modcombat; tbatch=batch}}else{mod = mod01}
+        
+        corrected_batch_only_par = CComBat(full_matrix1, batch = tbatch, mod = mod, par.prior = param)
         write.table(corrected_batch_only_par, 
                     file=paste("/media/koekiemonster/DATA-FAST/genetic_expression/hackathon_2/Lung/_prepped/methylation_combat_0nans_primaryonly_",
-                               parstring, "_target1.csv", sep=""),
+                               parstring, "_",tstring,"1.csv", sep=""),
                     row.names=TRUE)
         
-        corrected_batch_only_par = CComBat(full_matrix2, batch = batch2, mod = mod02, par.prior = param)
+        if(nullmode==FALSE){if(splitByTarget==TRUE){mod = modcombat2; tbatch=batch2}else{mod = modcombat; tbatch=batch}}else{mod = mod02}
+        corrected_batch_only_par = CComBat(full_matrix2, batch = tbatch, mod = mod, par.prior = param)
         write.table(corrected_batch_only_par, 
                     file=paste("/media/koekiemonster/DATA-FAST/genetic_expression/hackathon_2/Lung/_prepped/methylation_combat_0nans_primaryonly_",
-                               parstring, "_target2.csv", sep=""),
+                               parstring, "_",tstring,"2.csv", sep=""),
                   row.names=TRUE)
       }
-    }else{
-      if (splitByType==FALSE){
-        corrected_all_parametric = CComBat(full_matrix, batch = batch, mod = modcombat, par.prior = param)
-        write.table(corrected_all_parametric, 
-                    file=paste("/media/koekiemonster/DATA-FAST/genetic_expression/hackathon_2/Lung/_prepped/methylation_combat_0nans_primaryonly_",
-                               parstring, "_gender.csv", sep=""), 
-                    row.names=TRUE)
-      }else{
-        corrected_all_parametric = CComBat(full_matrix1, batch = batch1, mod = modcombat1, par.prior = param)
-        write.table(corrected_all_parametric, 
-                    file=paste("/media/koekiemonster/DATA-FAST/genetic_expression/hackathon_2/Lung/_prepped/methylation_combat_0nans_primaryonly_",
-                               parstring, "_gender_target1.csv", sep=""), 
-                    row.names=TRUE)
-        
-        corrected_all_parametric = CComBat(full_matrix2, batch = batch2, mod = modcombat2, par.prior = param)
-        write.table(corrected_all_parametric, 
-                    file=paste("/media/koekiemonster/DATA-FAST/genetic_expression/hackathon_2/Lung/_prepped/methylation_combat_0nans_primaryonly_",
-                               parstring, "_gender_target2.csv", sep=""),
-                    row.names=TRUE)
-      }
-    }
-
+ 
