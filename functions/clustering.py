b2142af1dab8e584b92e57465cc546e3d558d4d0
Bram van Es <bramiozo@gmail.com>
2020-02-16 19:10:42 +0100

    updatE

---
+++ functions/clustering.py
@@ -0,0 +1,47 @@
+class SimilarityConstructor:
+    def __init__():
+        
+        
+        return True
+
+class MarkovClustering:
+'''
+
+'''
+    def __init__():
+        
+        
+        return True
+
+    
+    def fit():
+        
+        return self
+        
+    def transform():
+        
+        return clusters
+
+    
+    @jit
+    def _inflation():
+        '''
+        
+        '''
+        
+        
+        return True
+    
+    @jit
+    def _expansion():
+        
+        return True
+
+    
+
+class AffinityPropagation:
+    def __init__():
+        
+        return True
+    
+    
--- functions/predictor_ebe.py
+++ functions/predictor_ebe.py
@@ -3,32 +3,31 @@
 
 class CEBE():
 	def __init__(self, clustering = "SOM", dim_reduction = None, aggregations = None, classifier = "LSVM"):
-
-# Clustering
-####################################
-# SOM, AP
-
+    # Clustering
+    ####################################
+    # SOM, AP
 
 
 
-# Dim. reduction
-####################################
-# Variance filter per cluster?
+
+    # Dim. reduction
+    ####################################
+    # Variance filter per cluster?
 
 
 
-# Sample aggregations per cluster
-####################################
-# percentiles: 10, 25, 50, 75, 90, modes: peaks in density estimations
-# percentiles per class, per feature, per cluster,
-# [cluster_n, classification_m, percentiles, ..]
+    # Sample aggregations per cluster
+    ####################################
+    # percentiles: 10, 25, 50, 75, 90, modes: peaks in density estimations
+    # percentiles per class, per feature, per cluster,
+    # [cluster_n, classification_m, percentiles, ..]
 
 
 
-# Convex Hulls
-####################################
-# 
-# [hull_k, cluster_n, classification_m, hull_coordinates_mean, hull_coordinates_median, hull_coordinates_variance]
+    # Convex Hulls
+    ####################################
+    # 
+    # [hull_k, cluster_n, classification_m, hull_coordinates_mean, hull_coordinates_median, hull_coordinates_variance]
 
 
 
